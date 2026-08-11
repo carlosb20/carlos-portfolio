516540f28f97a8f525c400d3ba5e4ddcb688e289
carlos alberto <carlosalbertosnb20@gmail.com>
2026-08-11 00:11:45 -0300

    fefdf

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -96,7 +96,7 @@ function Sobre() {
                 <section className={styles.contProjetos}>
                     <nav>
                         <ul>
-                            <li><a href="https://pizzaria-fxkv.onrender.com">Projeto pizzaria </a></li>
+                            <li><a href="dfd"> link </a></li>
                             <li><a href="dfd"> link </a></li>
                             <li><a href="dfd"> link </a></li>
                             <li><a href="dfd"> link </a></li>
